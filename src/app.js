97d060e19c49bc5a887f737b4bf1f3dbf0bae8c8
chris911e <christopherscheel99@gmail.com>
2022-05-27 14:02:38 +0200

    with .env vars

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
+require("dotenv/config")
+
 const express = require("express")
 const path = require("path")
 const app = express()
-const port = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 
@@ -9,6 +10,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/public/views/index.html'))
 })
 
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT}`)
 })
