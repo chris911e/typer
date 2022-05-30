154d14a9ae1afc16557a567edac5ba0d857d2a99
Christopher Scheel <christopherscheel99@gmail.com>
2022-05-30 17:14:26 +0200

    added for loop

--- src/public/modules/typer.js
+++ src/public/modules/typer.js
@@ -1,8 +1,18 @@
+import * as fs from "fs"
 // const input = document.querySelector(".input")
 // const typistWrapper = document.querySelector(".typist-wrapper")
 
 
 export function fillStr(str, mode, count){
+    fs.readFile('wordlists.json', 'utf-8', (err, myData) => {
+        if (err) {
+            console.log(err)
+        }
+    })
+
+    for(let i = 0; i < count; i++){
+        str += myData[`${mode}`]["words"][Math.floor(Math.random() * myData[`${mode}`]["words"].length)]
+    }
     
 }
 
