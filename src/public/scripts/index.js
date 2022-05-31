a65bd0663c3d46026f2064257ef6cfb40b1abce6
Christopher Scheel <christopherscheel99@gmail.com>
2022-05-31 11:45:23 +0200

    started options function

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,7 +1,14 @@
 import * as typer from '../modules/typer.js'
 
 const text = document.querySelector(".text")
+const input = document.querySelector(".input")
+const menuWrapper = document.querySelector(".menu-wrapper")
+const typistWrapper = document.querySelector(".typist-wrapper")
 
 var str = "";
 var strChars = []
 
+
+function options(){
+    document.getElementsByClassName("pop-up")[0].style.visibility = "visible"
+}
