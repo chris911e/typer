3b86ee8da79cd8c9b300597997cfcc4f3dd44520
chris911e <christopherscheel99@gmail.com>
2022-06-05 00:48:43 +0200

    added eventlisteners to select

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -2,15 +2,42 @@ import * as typer from '../modules/typer.js'
 
 function options() {
     typer.pop_up.style.visibility = "visible"
+    
+    switch(typer.option.value){
+        case "nothing":   
+            typer.option_input.style.visibility = "hidden"
+            typer.type_button.style.visibility = "hidden"
+            break
+        default:   
+            typer.option_input.style.visibility = "visible"
+            typer.type_button.style.visibility = "visible"
+            break
+    }
+
+    typer.option_input.style.visibility = "visible"
+    typer.type_button.style.visibility = "visible"
     typer.html.style.backgroundColor = "rgb(10, 50, 73)"
     typer.input.style.backgroundColor = "rgb(10, 50, 73)"
 }
 
 function xfunc() {
     typer.pop_up.style.visibility = "hidden"
-    typer.html.style.backgroundColor = "rgb(41, 93, 123)"
+    typer.option_input.style.visibility = "hidden"
+    typer.type_button.style.visibility = "hidden"
+    typer.html.style.backgroundColor = "rgb(41, 93, 123)" 
     typer.input.style.backgroundColor = "rgb(41, 93, 123)"
 }
 
-document.querySelector('.x-button').addEventListener('click', xfunc)
-document.querySelector('.option-button').addEventListener('click', options)
+
+typer.option.addEventListener('change', (event) => {
+    if(typer.option.value == "words" || typer.option.value == "time"){
+        typer.option_input.style.visibility = "visible"
+        typer.type_button.style.visibility = "visible"
+    } else if(typer.option.value == "nothing"){
+        typer.option_input.style.visibility = "hidden"
+        typer.type_button.style.visibility = "hidden"
+    }
+})
+
+typer.option_button.addEventListener('click', options)
+typer.x_button.addEventListener('click', xfunc)
