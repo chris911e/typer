edb565149a7897d1d67e946eb56b0ea11123f48d
chris911e <christopherscheel99@gmail.com>
2022-06-05 15:30:33 +0200

    added visibility settings

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -2,20 +2,9 @@ import * as typer from '../modules/typer.js'
 
 function options() {
     typer.pop_up.style.visibility = "visible"
-    
-    switch(typer.option.value){
-        case "nothing":   
-            typer.option_input.style.visibility = "hidden"
-            typer.type_button.style.visibility = "hidden"
-            break
-        default:   
-            typer.option_input.style.visibility = "visible"
-            typer.type_button.style.visibility = "visible"
-            break
-    }
-
-    typer.option_input.style.visibility = "visible"
-    typer.type_button.style.visibility = "visible"
+    typer.option.value = "nothing"
+    typer.option_input.style.visibility = "hidden"
+    typer.type_button.style.visibility = "hidden"
     typer.html.style.backgroundColor = "rgb(10, 50, 73)"
     typer.input.style.backgroundColor = "rgb(10, 50, 73)"
 }
@@ -24,8 +13,13 @@ function xfunc() {
     typer.pop_up.style.visibility = "hidden"
     typer.option_input.style.visibility = "hidden"
     typer.type_button.style.visibility = "hidden"
+    typer.option_suffix.style.visibility = "hidden"
     typer.html.style.backgroundColor = "rgb(41, 93, 123)" 
     typer.input.style.backgroundColor = "rgb(41, 93, 123)"
+}
+
+function type() {
+    type
 }
 
 
@@ -33,11 +27,25 @@ typer.option.addEventListener('change', (event) => {
     if(typer.option.value == "words" || typer.option.value == "time"){
         typer.option_input.style.visibility = "visible"
         typer.type_button.style.visibility = "visible"
+        typer.option_suffix.style.visibility = "visible"
+        
+        switch(typer.option.value){
+            case "words":
+                typer.option_suffix.innerHTML = "words"
+                break
+            case "time":
+                typer.option_suffix.innerHTML = "seconds"
+                break
+        }
+        
+        typer.option_input.focus()
     } else if(typer.option.value == "nothing"){
+        typer.option_suffix.style.visibility = "hidden"
         typer.option_input.style.visibility = "hidden"
         typer.type_button.style.visibility = "hidden"
     }
 })
+typer.type_button.addEventListener('click', type)
 
 typer.option_button.addEventListener('click', options)
 typer.x_button.addEventListener('click', xfunc)
