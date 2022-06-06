d31d72fd884bef9811f6b8d9da7ab249dfeac5c8
chris911e <christopherscheel99@gmail.com>
2022-06-06 16:41:14 +0200

    added visibility stylign

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -3,6 +3,7 @@ import * as typer from '../modules/typer.js'
 function options() {
     typer.pop_up.style.visibility = "visible"
     typer.option.value = "nothing"
+    typer.mode_option.value = "nothing"
     typer.option_input.style.visibility = "hidden"
     typer.type_button.style.visibility = "hidden"
     typer.html.style.backgroundColor = "rgb(10, 50, 73)"
@@ -14,12 +15,13 @@ function xfunc() {
     typer.option_input.style.visibility = "hidden"
     typer.type_button.style.visibility = "hidden"
     typer.option_suffix.style.visibility = "hidden"
-    typer.html.style.backgroundColor = "rgb(41, 93, 123)" 
+    typer.mode_option.style.visibility = "hidden"
+    typer.html.style.backgroundColor = "rgb(41, 93, 123)"
     typer.input.style.backgroundColor = "rgb(41, 93, 123)"
 }
 
 function type() {
-    type
+    
 }
 
 
@@ -28,6 +30,7 @@ typer.option.addEventListener('change', (event) => {
         typer.option_input.style.visibility = "visible"
         typer.type_button.style.visibility = "visible"
         typer.option_suffix.style.visibility = "visible"
+        typer.mode_option.style.visibility = "visible"
         
         switch(typer.option.value){
             case "words":
@@ -43,6 +46,7 @@ typer.option.addEventListener('change', (event) => {
         typer.option_suffix.style.visibility = "hidden"
         typer.option_input.style.visibility = "hidden"
         typer.type_button.style.visibility = "hidden"
+        typer.mode_option.style.visibility = "hidden"
     }
 })
 typer.type_button.addEventListener('click', type)
