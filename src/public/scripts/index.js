4afb1b92628d2beca5d6647a25a3a50e64970b55
Christopher Scheel <christopherscheel99@gmail.com>
2022-06-07 11:43:51 +0200

    implemented frontend while typing logic

--- src/public/scripts/index.js
+++ src/public/scripts/index.js
@@ -1,6 +1,6 @@
 import * as typer from '../modules/typer.js'
 
-var difficulty, amount
+var difficulty, amount, option_mode
 
 function options() {
     typer.pop_up.style.visibility = "visible"
@@ -25,9 +25,11 @@ function xfunc() {
 function type() {
     difficulty = typer.mode_option.value
     amount = parseInt(typer.option_input.value)
-    switch(typer.option.value){
+    option_mode = typer.option.value
+    switch(option_mode){
         case "time":
             typer.fillStr(difficulty, amount * 5)
+            typer.time = amount
             typer.renderText(typer.str)
             break;
         case "words":
@@ -35,6 +37,8 @@ function type() {
             typer.renderText(typer.str)
             break;
     }
+    typer.startTyping()
+
     typer.pop_up.style.visibility = "hidden"
     typer.option_input.style.visibility = "hidden"
     typer.type_button.style.visibility = "hidden"
@@ -44,6 +48,11 @@ function type() {
     typer.input.style.backgroundColor = "rgb(41, 93, 123)"
 }
 
+function restart(){
+    typer.str = ""
+    type()
+}
+
 
 typer.option.addEventListener('change', (event) => {
     if(typer.option.value == "words" || typer.option.value == "time"){
@@ -51,7 +60,7 @@ typer.option.addEventListener('change', (event) => {
         typer.type_button.style.visibility = "visible"
         typer.option_suffix.style.visibility = "visible"
         typer.mode_option.style.visibility = "visible"
-        
+
         switch(typer.option.value){
             case "words":
                 typer.option_suffix.innerHTML = "words"
@@ -60,8 +69,8 @@ typer.option.addEventListener('change', (event) => {
                 typer.option_suffix.innerHTML = "seconds"
                 break
         }
-        
         typer.option_input.focus()
+
     } else if(typer.option.value == "nothing"){
         typer.option_suffix.style.visibility = "hidden"
         typer.option_input.style.visibility = "hidden"
@@ -74,8 +83,8 @@ typer.type_button.addEventListener('click', type)
 
 window.addEventListener('load', (event) => {
     typer.option.value = "words"
-    typer.mode_option.value = "normal"
-    typer.option_input.value = 69
+    typer.mode_option.value = "easy"
+    typer.option_input.value = 5
     type()
 })
 
