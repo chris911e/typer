38aab22281a1eb1f4df9a67d893f56b080d45bcb
Christopher Scheel <christopherscheel99@gmail.com>
2022-06-07 11:43:31 +0200

    implemented typing logic

--- src/public/modules/typer.js
+++ src/public/modules/typer.js
@@ -5,6 +5,8 @@ export const input = document.querySelector(".input")
 export const menuWrapper = document.querySelector(".menu-wrapper")
 export const typistWrapper = document.querySelector(".typist-wrapper")
 
+export const stats = document.querySelector(".stats")
+
 export const pop_up = document.querySelector(".pop-up")
 export const html = document.getElementsByTagName("html")[0]
 
@@ -16,8 +18,13 @@ export const type_button = document.querySelector(".type-button")
 
 export const option_suffix = document.querySelector(".option-suffix")
 export const mode_option = document.querySelector(".mode-option")
+
 export var str = ""
 export var strChars = []
+export var time = null
+export var success = null
+export var errors = 0
+export var wpm = 0
 
 export function fillStr(mode, count){
     var prev = ""
@@ -50,11 +57,34 @@ export function resetStrChars(){
     renderText(str)
 }
 
-export function type(){
-    input.addEventListener("keyup", () => {
-        if(!timer){
-            timer = setTimeout()
+export function startTyping(){
+    input.addEventListener('keyup', (event) => {
+        stats.style.visibility = "visible"
+        if(time != null){
+            timer = setTimeout(() => {
+                success = false
+            }, time);
+        }
+        var input_value = input.value
+        var index = input_value.length-1
+        input_value.split("").map((letter, i) => {
+            if(letter == str[i]){
+                strChars[i].classList.add("correct")
+            }else{
+                strChars[i].classList.add("wrong")
+            }
+
+            switch(event.key){
+                case "Backspace":
+                    strChars[i+1].classList.remove("wrong")
+                    strChars[i+1].classList.remove("correct")
+                case "Enter":
+                    event.preventDefault()
+            }
+        })
+
+        if(input_value.length == str.length){
+            success = true
         }
     })
 }
-
