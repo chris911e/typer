0070141e7c3bc6bf43c901fc2ef30ace7175dc4f
Christopher Scheel <christopherscheel99@gmail.com>
2022-06-08 16:34:39 +0200

    changed express import

--- src/app.js
+++ src/app.js
@@ -4,8 +4,9 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-import express from "express"
 import * as path from "path"
+
+import express from 'express';
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
